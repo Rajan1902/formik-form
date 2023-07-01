c0517d32383effb7ae987e929ede7bba8e36329d
Rajan1902 <rajansinghoo150@gmail.com>
2023-07-01 20:57:07 +0530

    initial commit

---
+++ src/Registration.jsx
@@ -0,0 +1,516 @@
+import React from "react";
+import {
+  TextField,
+  TextareaAutosize,
+  FormControl,
+  InputLabel,
+  Select,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+  Checkbox,
+  MenuItem,
+  Button,
+} from "@mui/material";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const Registration = () => {
+  const countries = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antigua &amp; Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia",
+    "Bosnia &amp; Herzegovina",
+    "Botswana",
+    "Brazil",
+    "British Virgin Islands",
+    "Brunei",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cambodia",
+    "Cameroon",
+    "Cape Verde",
+    "Cayman Islands",
+    "Chad",
+    "Chile",
+    "China",
+    "Colombia",
+    "Congo",
+    "Cook Islands",
+    "Costa Rica",
+    "Cote D Ivoire",
+    "Croatia",
+    "Cruise Ship",
+    "Cuba",
+    "Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Estonia",
+    "Ethiopia",
+    "Falkland Islands",
+    "Faroe Islands",
+    "Fiji",
+    "Finland",
+    "France",
+    "French Polynesia",
+    "French West Indies",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Gibraltar",
+    "Greece",
+    "Greenland",
+    "Grenada",
+    "Guam",
+    "Guatemala",
+    "Guernsey",
+    "Guinea",
+    "Guinea Bissau",
+    "Guyana",
+    "Haiti",
+    "Honduras",
+    "Hong Kong",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland",
+    "Isle of Man",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jersey",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kuwait",
+    "Kyrgyz Republic",
+    "Laos",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macau",
+    "Macedonia",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Moldova",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Montserrat",
+    "Morocco",
+    "Mozambique",
+    "Namibia",
+    "Nepal",
+    "Netherlands",
+    "Netherlands Antilles",
+    "New Caledonia",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palestine",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Puerto Rico",
+    "Qatar",
+    "Reunion",
+    "Romania",
+    "Russia",
+    "Rwanda",
+    "Saint Pierre &amp; Miquelon",
+    "Samoa",
+    "San Marino",
+    "Satellite",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "South Africa",
+    "South Korea",
+    "Spain",
+    "Sri Lanka",
+    "St Kitts &amp; Nevis",
+    "St Lucia",
+    "St Vincent",
+    "St. Lucia",
+    "Sudan",
+    "Suriname",
+    "Swaziland",
+    "Sweden",
+    "Switzerland",
+    "Syria",
+    "Taiwan",
+    "Tajikistan",
+    "Tanzania",
+    "Thailand",
+    "Timor L'Este",
+    "Togo",
+    "Tonga",
+    "Trinidad &amp; Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Turks &amp; Caicos",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "Uruguay",
+    "Uzbekistan",
+    "Venezuela",
+    "Vietnam",
+    "Virgin Islands (US)",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+  ];
+  const hobbiesList = [
+    "3D printing",
+    "Amateur radio",
+    "Scrapbook",
+    "Amateur radio",
+    "Acting",
+    "Baton twirling",
+    "Board games",
+    "Book restoration",
+    "Cabaret",
+    "Calligraphy",
+    "Candle making",
+    "Computer programming",
+    "Coffee roasting",
+    "Cooking",
+    "Colouring",
+    "Cosplaying",
+    "Couponing",
+    "Creative writing",
+    "Crocheting",
+    "Cryptography",
+    "Dance",
+    "Digital arts",
+    "Drama",
+    "Drawing",
+    "Do it yourself",
+    "Electronics",
+    "Embroidery",
+    "Fashion",
+    "Flower arranging",
+    "Foreign language learning",
+    "Gaming",
+    "Tabletop games",
+    "Role-playing games",
+    "Gambling",
+    "Genealogy",
+    "Glassblowing",
+    "Gunsmithing",
+    "Homebrewing",
+    "Ice skating",
+    "Jewelry making",
+    "Jigsaw puzzles",
+    "Juggling",
+    "Knapping",
+    "Knitting",
+    "Kabaddi",
+    "Knife making",
+    "Lacemaking",
+    "Lapidary",
+    "Leather crafting",
+    "Lego building",
+    "Lockpicking",
+    "Machining",
+    "Macrame",
+    "Metalworking",
+    "Magic",
+    "Model building",
+    "Listening to music",
+    "Origami",
+    "Painting",
+    "Playing musical instruments",
+    "Pet",
+    "Poi",
+    "Pottery",
+    "Puzzles",
+    "Quilting",
+    "Reading",
+    "Scrapbooking",
+    "Sculpting",
+    "Sewing",
+    "Singing",
+    "Sketching",
+    "Soapmaking",
+    "Sports",
+    "Stand-up comedy",
+    "Sudoku",
+    "Table tennis",
+    "Taxidermy",
+    "Video gaming",
+    "Watching movies",
+    "Web surfing",
+    "Whittling",
+    "Wood carving",
+    "Woodworking",
+    "World Building",
+    "Writing",
+    "Yoga",
+    "Yo-yoing",
+    "Air sports",
+    "Archery",
+    "Astronomy",
+    "Backpacking",
+    "Base jumping",
+    "Baseball",
+    "Basketball",
+    "Beekeeping",
+    "Bird watching",
+    "Blacksmithing",
+    "Board sports",
+    "Bodybuilding",
+    "Brazilian jiu-jitsu",
+    "Community",
+    "Cycling",
+    "Dowsing",
+    "Driving",
+    "Fishing",
+    "Flag football",
+    "Flying",
+    "Flying disc",
+    "Foraging",
+    "Gardening",
+    "Geocaching",
+    "Ghost hunting",
+    "Graffiti",
+    "Handball",
+    "Hiking",
+    "Hooping",
+    "Horseback riding",
+    "Hunting",
+    "Inline skating",
+    "Jogging",
+    "Kayaking",
+    "Kite flying",
+    "Kitesurfing",
+    "Larping",
+    "Letterboxing",
+    "Metal detecting",
+    "Motor sports",
+    "Mountain biking",
+    "Mountaineering",
+    "Mushroom hunting",
+    "Mycology",
+    "Netball",
+    "Nordic skating",
+    "Orienteering",
+    "Paintball",
+    "Parkour",
+    "Photography",
+    "Polo",
+    "Rafting",
+    "Rappelling",
+    "Rock climbing",
+    "Roller skating",
+    "Rugby",
+    "Running",
+    "Sailing",
+    "Sand art",
+    "Scouting",
+    "Scuba diving",
+    "Sculling",
+    "Rowing",
+    "Shooting",
+    "Shopping",
+    "Skateboarding",
+    "Skiing",
+    "Skim Boarding",
+    "Skydiving",
+    "Slacklining",
+    "Snowboarding",
+    "Stone skipping",
+    "Surfing",
+    "Swimming",
+    "Taekwondo",
+    "Tai chi",
+    "Urban exploration",
+    "Vacation",
+    "Vehicle restoration",
+    "Water sports",
+  ];
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Name is required"),
+    address: Yup.string().required("Address is required"),
+    country: Yup.string().required("Country is required"),
+    gender: Yup.string().required("Gender is required"),
+    hobbies: Yup.array().min(1, "Select at least one hobby"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      address: "",
+      country: "",
+      gender: "",
+      hobbies: [],
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert("form successfully sumbitted");
+      console.log("Registration submitted!");
+      console.log("Name:", values.name);
+      console.log("Address:", values.address);
+      console.log("Country:", values.country);
+      console.log("Gender:", values.gender);
+      console.log("Hobbies:", values.hobbies);
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <TextField
+        label="Name"
+        name="name"
+        value={formik.values.name}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.name && Boolean(formik.errors.name)}
+        helperText={formik.touched.name && formik.errors.name}
+        fullWidth
+        margin="normal"
+      />
+
+      <TextareaAutosize
+        minRows={4}
+        placeholder="Address"
+        name="address"
+        value={formik.values.address}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.address && Boolean(formik.errors.address)}
+        helperText={formik.touched.address && formik.errors.address}
+        fullWidth
+        margin="normal"
+      />
+
+      <FormControl fullWidth margin="normal">
+        <InputLabel>Country</InputLabel>
+        <Select
+          name="country"
+          value={formik.values.country}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.country && Boolean(formik.errors.country)}
+        >
+          {countries.map((countryOption) => (
+            <MenuItem key={countryOption} value={countryOption}>
+              {countryOption}
+            </MenuItem>
+          ))}
+        </Select>
+        {formik.touched.country && formik.errors.country && (
+          <div className="error">{formik.errors.country}</div>
+        )}
+      </FormControl>
+
+      <FormControl component="fieldset" fullWidth margin="normal">
+        <RadioGroup
+          name="gender"
+          value={formik.values.gender}
+          onChange={formik.handleChange}
+        >
+          <FormControlLabel value="male" control={<Radio />} label="Male" />
+          <FormControlLabel value="female" control={<Radio />} label="Female" />
+          <FormControlLabel value="other" control={<Radio />} label="Other" />
+        </RadioGroup>
+      </FormControl>
+      {formik.touched.gender && formik.errors.gender && (
+        <div className="error">{formik.errors.gender}</div>
+      )}
+
+      <FormControl fullWidth margin="normal">
+        <InputLabel>Hobbies/Interests</InputLabel>
+        <Select
+          multiple
+          name="hobbies"
+          value={formik.values.hobbies}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          renderValue={(selected) => selected.join(", ")}
+          error={formik.touched.hobbies && Boolean(formik.errors.hobbies)}
+        >
+          {hobbiesList.map((hobby) => (
+            <MenuItem key={hobby} value={hobby}>
+              <Checkbox checked={formik.values.hobbies.indexOf(hobby) > -1} />
+              {hobby}
+            </MenuItem>
+          ))}
+        </Select>
+        {formik.touched.hobbies && formik.errors.hobbies && (
+          <div className="error">{formik.errors.hobbies}</div>
+        )}
+      </FormControl>
+
+      <Button type="submit" variant="contained" color="primary">
+        Register
+      </Button>
+    </form>
+  );
+};
+
+export default Registration;
